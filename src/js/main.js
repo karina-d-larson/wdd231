828a031d0cf56aa55864172c5fcec296088c42ff
karina-d-larson <karina.d.larson@gmail.com>
2026-04-29 11:23:26 -0600

    finishing the styling week 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-import { getParkData } from "./parkService.mjs";
 
-const parkData = getParkData();
+
+function loadParkData() {
+
+  document.getElementById("parkName").textContent = "Yellowstone";
+  document.getElementById("parkType").textContent = "National Park";
+  document.getElementById("parkStates").textContent = "WY, ID, MT";
+  document.querySelector("#hero-image").src = "./images/yellowstone.jpg";
+  document.querySelector("#hero-image").alt = "Yellowstone";
+
+}
+
+loadParkData();
